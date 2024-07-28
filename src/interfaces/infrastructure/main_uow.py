66dff94d1c48ae204409c9d9f6e4a677b2b19e09
mutherpy <googlish97@gmail.com>
2024-07-28 04:04:59 +0300

    fix: remove redundant interfaces

--- src/interfaces/infrastructure/main_uow.py
+++
@@ -1,7 +0,0 @@
-from interfaces.infrastructure.uow import IBaseUOW
-from abc import ABC
-from interfaces.infrastructure.uows.sql_db import IDBUoW
-
-
-class IMainUOW(IBaseUOW, ABC):
-    db: IDBUoW
--- src/interfaces/infrastructure/repositories/query/user.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC
-
-
-class IUserQueryRepository(ABC):
-
-    async def retrieve_by_username(self, username):
-        raise NotImplementedError
