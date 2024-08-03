984998c77c3e152ac39f9e6f768487e925d333b1
mutherpy <googlish97@gmail.com>
2024-08-04 01:17:04 +0300

    fix: - return auth +

--- src/providers/implementations/auth_plugs.py
+++ src/providers/implementations/auth_plugs.py
@@ -13,11 +13,9 @@ sec = HTTPBearer(auto_error=False)
 
 
 async def get_current_user(
-        # creds: HTTPAuthorizationCredentials = Depends(sec),
+        creds: HTTPAuthorizationCredentials = Depends(sec),
         db_uow=Depends(sql_db_uow_provider)
 ) -> Optional[AuthUserDTO]:
-    # token_data: TokenDataDTO = IdentityProvider.validate_token(creds.credentials)
-    token_data: TokenDataDTO = IdentityProvider.validate_token('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsIm5ldyI6ZmFsc2V9.jyIhM3aQHJ-gGIwwMNA_ruzhwtAAq58sEcYc7UuwlhE')
-    print(token_data)
+    token_data: TokenDataDTO = IdentityProvider.validate_token(creds.credentials)
     auth_user_dto: Optional[AuthUserDTO] = await db_uow.user_q_rep.retrieve_id_by_username(username=token_data.username)
     return auth_user_dto
